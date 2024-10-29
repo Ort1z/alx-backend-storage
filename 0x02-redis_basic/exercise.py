ed5575874b54590a0a4998e3c6ce99b47b805ec2
MASINGITA OTTIS MALULEKE <38721983+Ort1z@users.noreply.github.com>
2024-10-29 13:08:04 +0000

    Updates

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,80 +1,81 @@
 #!/usr/bin/env python3
+"""
+Redis basic exercises - implementing a Cache class with counting decorator
+"""
 import redis
-import uuid
 from typing import Union, Callable, Optional
 from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
-    ''' def count calls '''
+    """
+    Decorator that counts how many times a method is called.
+    
+    Args:
+        method: The method to be decorated
+        
+    Returns:
+        Callable: The wrapped function
+    """
     @wraps(method)
-    def wrapper(self, *args, **kwds):
-        ''' def wrapper '''
-        key_name = method.__qualname__
-        self._redis.incr(key_name, 0) + 1
-        return method(self, *args, **kwds)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper function that increments the call count and executes the method.
+        
+        Args:
+            self: Instance of the Cache class
+            *args: Variable positional arguments
+            **kwargs: Variable keyword arguments
+            
+        Returns:
+            Any: Result from the wrapped method
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
     return wrapper
 
 
-def call_history(method: Callable) -> Callable:
-    ''' def call history '''
-    @wraps(method)
-    def wrapper(self, *args, **kwds):
-        ''' def wrapper'''
-        key_m = method.__qualname__
-        inp_m = key_m + ':inputs'
-        outp_m = key_m + ":outputs"
-        data = str(args)
-        self._redis.rpush(inp_m, data)
-        fin = method(self, *args, **kwds)
-        self._redis.rpush(outp_m, str(fin))
-        return fin
-    return wrapper
-
-
-def replay(func: Callable):
-    '''def replay'''
-    r = redis.Redis()
-    key_m = func.__qualname__
-    inp_m = r.lrange("{}:inputs".format(key_m), 0, -1)
-    outp_m = r.lrange("{}:outputs".format(key_m), 0, -1)
-    calls_number = len(inp_m)
-    times_str = 'times'
-    if calls_number == 1:
-        times_str = 'time'
-    fin = '{} was called {} {}:'.format(key_m, calls_number, times_str)
-    print(fin)
-    for k, v in zip(inp_m, outp_m):
-        fin = '{}(*{}) -> {}'.format(
-            key_m,
-            k.decode('utf-8'),
-            v.decode('utf-8')
-        )
-        print(fin)
-
-
-class Cache():
-    ''' class cache '''
-    def __init__(self):
-        ''' def init '''
+class Cache:
+    """
+    Cache class to handle redis operations
+    """
+    def __init__(self) -> None:
+        """
+        Initialize the Cache instance.
+        Creates a Redis client and flushes the database.
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        ''' def store '''
-        generate = str(uuid.uuid4())
-        self._redis.set(generate, data)
-        return generate
+        """
+        Store data in Redis using a random key.
+        
+        Args:
+            data: Data to be stored (can be str, bytes, int, or float)
+            
+        Returns:
+            str: Key under which the data was stored
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
 
-    def get(self, key: str,
+    def get(self, key: str, 
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-        ''' def get '''
-        value = self._redis.get(key)
-        return value if not fn else fn(value)
-
-    def get_int(self, key):
-        return self.get(key, int)
-
-    def get_str(self, key):
-        value = self._redis.get(key)
-        return value.decode("utf-8")
+        """
+        Retrieve data from Redis.
+        
+        Args:
+            key: Key to look up in Redis
+            fn: Optional callable to transform the retrieved data
+            
+        Returns:
+            Union[str, bytes, int, float]: Retrieved data
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
